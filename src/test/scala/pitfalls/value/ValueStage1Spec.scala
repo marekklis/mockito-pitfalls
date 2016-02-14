326d2e426414dfe353f0db3245991a732170ee9a
Marek Kliś <marek.klis@scalac.io>
2016-02-14 13:30:22 +0100

    ValueStage1Spec test added

---
+++ src/test/scala/pitfalls/value/ValueStage1Spec.scala
@@ -0,0 +1,32 @@
+package pitfalls.value
+
+import org.mockito.BDDMockito._
+import org.scalatest._
+import org.scalatest.mock.MockitoSugar
+import pitfalls.value.Value.StringValue
+
+class ValueStage1Spec extends FunSpec with MockitoSugar {
+
+  val valueService = mock[ValueService]
+
+  describe("While dealing with value object") {
+
+    describe("Mockito doesn't work with matcher") {
+
+      it("for String") {
+
+        val first = StringValue("first")
+
+        given(valueService.doItString(
+          org.mockito.Matchers.eq(first),
+          org.mockito.Matchers.any[StringValue]))
+          .willReturn("lorem ipsum")
+
+        val result = valueService.doItString(first, StringValue("second"))
+
+        assert(result == "lorem ipsum")
+      }
+    }
+  }
+
+}
