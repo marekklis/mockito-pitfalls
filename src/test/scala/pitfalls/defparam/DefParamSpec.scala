9848816ad7c4d4f585bec751a315a28c069318d0
Marek Kliś <marek.klis@scalac.io>
2016-02-14 11:11:43 +0100

    refactor - move "production" code into spec file

--- src/test/scala/pitfalls/defparam/DefParamSpec.scala
+++ src/test/scala/pitfalls/defparam/DefParamSpec.scala
@@ -5,6 +5,27 @@ import org.scalatest._
 import org.scalatest.mock.MockitoSugar
 import org.mockito.BDDMockito._
 
+trait Rockable {
+
+  def rock1(someInt: Int, someString: String): Int
+
+  def rock2(someInt: Int, someString: String, someWithDefault: Long = 6L)
+
+}
+
+class Band(rockable: Rockable) {
+
+  def letsRock1(someInt: Int, someString: String): Int =
+    rockable.rock1(someInt, someString)
+
+  def letsRock2(someInt: Int, someString: String) =
+    rockable.rock2(someInt, someString)
+
+  def letsRock2WithThird(someInt: Int, someString: String) =
+    rockable.rock2(someInt, someString, 6)
+
+}
+
 class DefParamSpec extends FunSpec with MockitoSugar with Matchers {
 
   val someInt = 1
