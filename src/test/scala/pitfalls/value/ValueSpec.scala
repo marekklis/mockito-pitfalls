4694f9b60255178389c8cfe4bdd0e135bcc1f706
Marek Kliś <marek.klis@scalac.io>
2016-02-14 13:34:15 +0100

    ValueStage - troublesome code removed

--- src/test/scala/pitfalls/value/ValueSpec.scala
+++ src/test/scala/pitfalls/value/ValueSpec.scala
@@ -10,35 +10,15 @@ class ValueSpec extends FunSpec with MockitoSugar {
   val valueService = mock[ValueService]
 
   describe("While dealing with value object") {
-    describe("Mockito doesn't work with matcher") {
-      it("for String") {
-
-        val eqFirst: StringValue = org.mockito.Matchers.eq(StringValue("first"))
-        val anySecond: StringValue = org.mockito.Matchers.any[StringValue]
-
-        given(valueService.doItString(
-          eqFirst,
-          anySecond))
-          .willReturn("lorem ipsum")
-
-        val result = valueService.doItString(StringValue("first"), StringValue("second"))
-
-        assert(result == "lorem ipsum")
-
-      }
-    }
 
     describe("Mockito works correctly if matcher is 'inside' value object") {
 
       it("works for String") {
 
-        val eqFirst: StringValue = StringValue(org.mockito.Matchers.eq("first"))
-        val anySecond: StringValue = StringValue(org.mockito.Matchers.any[String])
-
         given(valueService.doItString(
-          eqFirst,
-          anySecond))
-          .willReturn("lorem ipsum")
+          StringValue(org.mockito.Matchers.eq("first")),
+          StringValue(org.mockito.Matchers.any[String]))
+        ).willReturn("lorem ipsum")
 
         val result = valueService.doItString(StringValue("first"), StringValue("second"))
 
@@ -47,13 +27,10 @@ class ValueSpec extends FunSpec with MockitoSugar {
 
       it("it works for Long") {
 
-        val eqFirst: LongValue = LongValue(org.mockito.Matchers.eq(1L))
-        val anySecond: LongValue = LongValue(org.mockito.Matchers.any[Long])
-
         given(valueService.doItLong(
-          eqFirst,
-          anySecond))
-          .willReturn(7)
+          LongValue(org.mockito.Matchers.eq(1L)),
+          LongValue(org.mockito.Matchers.any[Long]))
+        ).willReturn(7)
 
         val result = valueService.doItLong(LongValue(1), LongValue(3))
 
